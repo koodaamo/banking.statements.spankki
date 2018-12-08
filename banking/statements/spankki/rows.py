0f49ba497d0dd9965393356bb556380e1b6cd176
Petri Savolainen <petri@koodaamo.fi>
2018-12-08 02:01:02 +0200

    refactor row generation

--- banking/statements/spankki/rows.py
+++ banking/statements/spankki/rows.py
@@ -3,7 +3,7 @@ Transaction table row utilities
 
 """
 
-from pdfminer.layout import LTTextLine
+from pdfminer.layout import LTAnno
 
 from .pages import all_txt_elms
 from .txids import txid_elms
@@ -18,56 +18,70 @@ def rowcoordinates(elms):
     return tuple(zip(coords, coords[1:]))
 
 
-def rowcontents(elms):
-    txtelms = list(all_txt_elms(elms))
+def txtelmrows(elms, under=3, over=3):
+    "generate coordinate-bound text element rows (coordinates extended by under and over)"
+    txtelms = tuple(all_txt_elms(elms))
     for (upper, lower) in rowcoordinates(elms):
-        rowelms = [e for e in txtelms if e.y0 >= lower - 3 and e.y1 <= upper + 3]
-        rowelms.sort(key=lambda e: e.x0)
+        yield (e for e in txtelms if e.y0 >= lower - under and e.y1 <= upper + over)
 
-        if rowelms[0].get_text().strip().endswith("HOK-ELANTO"):
-            assert len(rowelms) == 4
-            rowelms.insert(2, LTTextLine(""))
+def normalizerow(row):
+    "fix row element count"
+    row = list(row)
+    row.sort(key=lambda e: e.x0)
+    size = len(row)
 
-        elif rowelms[4].get_text().strip()=="* JATKUU *":
-            del rowelms[4]
-            assert len(rowelms) == 6, [e.get_text().strip() for e in rowelms]
+    if size == 6:
+        pass
 
-        elif "TILISIIRTO" in rowelms[2].get_text().strip():
-            assert len(rowelms) == 8, [e.get_text().strip() for e in rowelms]
-        else:
-            assert len(rowelms) == 6, [e.get_text().strip() for e in rowelms]
+    # fix short row
+    elif size == 4 and row[0].get_text().strip().endswith("HOK-ELANTO"):
+        row.insert(2, LTAnno(""))
+        row.insert(2, LTAnno(""))
 
-        rowtxts = [e.get_text().strip() for e in rowelms]
-        rowtxts[-1] = rowtxts[-1].rstrip('-').replace(',', '.')
+    # fix special case
+    elif size == 7 and row[4].get_text().strip()=="* JATKUU *":
+        del row[4]
+
+    # fix special case (also has TILISIIRTO)
+    elif size == 8:
+        row0txt = row[0].get_text().strip()
+        if not row0txt.endswith(" A"):
+            combined = row0txt + " A " + row[1].get_text().strip()
+            row.insert(0, LTAnno(combined))
+            del row[1:3]
+
+        # merge automatic message elements and move them before the user-given one
+        combined = row[3].get_text().strip() + " " + row[4].get_text().strip()
+        del row[3:5]
+        row.insert(2, LTAnno(combined))
+
+    # merge the two message elements
+    combined = row[2].get_text().strip() + " " + row[3].get_text().strip()
+    del row[2:4]
+    row.insert(2, LTAnno(combined))
+
+    return row
 
 
-        if " A" not in rowtxts[0]:
-            rowtxts[0] = rowtxts[0] + " A " + rowtxts[1]
-            del rowtxts[1]
+def rowcontents(elms):
+    for txtelmrow in txtelmrows(elms):
+        row = normalizerow(txtelmrow)
 
-        # merge message fields
+        rowtxts = [e.get_text().strip() for e in row]
+        rowtxts[-1] = rowtxts[-1].replace(',', '.')
 
-        if "TILISIIRTO" in rowtxts[1]:
-            # custom case :(
-            rowtxts[2] = ' '.join(rowtxts[2:5])
-            del rowtxts[3:5]
-            assert len(rowtxts) == 5
-        else:
-            rowtxts[2] = rowtxts[2] + " " + rowtxts[3]
-            del rowtxts[3]
-
-        row = []
+        result = []
         for i, txt in enumerate(rowtxts):
             parts = [p.strip() for p in txt.split()]
 
             if i==0:
-                row.extend((parts[0] + ' ' + parts[1] , parts[2], ' '.join(parts[3:])))
+                result.extend((parts[0] + ' ' + parts[1] , parts[2], ' '.join(parts[3:])))
             elif i==1:
-                row.extend(parts)
+                result.extend(parts)
             else:
-                row.append(txt)
+                result.append(txt)
 
-        yield row
+        yield result
 
 
 def fixedrows(elms):
